00e7377841ccbf1e55d80f5c1fe8ed205a20ee1d
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2020-07-05 16:52:25 +0900

    言語のキーワードを定義

--- token/token.go
+++ token/token.go
@@ -32,3 +32,14 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET
+}
+
+func LookupIdent(ident string) TokenType{
+	if tok,ok := keywords[ident]; ok{
+		return tok
+	}
+}
